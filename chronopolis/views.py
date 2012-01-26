67f0a756c184373bdd019ef76b810b4bd6cb0e8b
Christopher Cope <Shadow6363@gmail.com>
2012-01-25 20:13:36 -0500

    Began create survey view.

--- chronopolis/views.py
+++ chronopolis/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
+from chronopolis.models import Survey, Question
+
+def create(request):
+	
